83f6f3179f889284a2cdc1dc0b6914f09964984e
root <root@impulcetto.localdomain>
2020-08-17 18:03:36 -0300

    Create Nasa Class, Refactoring Code

--- java/exploringMars/src/main/java/br/com/elo7/exploringMars/App.java
+++ java/exploringMars/src/main/java/br/com/elo7/exploringMars/App.java
@@ -16,56 +16,50 @@ public class App
     	int axisX = scan.nextInt();
     	int axisY = scan.nextInt();
 		
-		CartesianPlan cp = new CartesianPlan(axisX, axisY);
+		CartesianPlan cp = Nasa.cartesianPlan(axisX, axisY);
 		
 			
-		int initX01 = scan.nextInt();
-		int initY01 = scan.nextInt();
-		String direction01 = scan.next();
-		Probe p1 = new Probe(initX01, initY01, direction01);
+		int initX = scan.nextInt();
+		int initY = scan.nextInt();
+		String direction = scan.next();
+		Probe p1 = Nasa.probe(initX, initY, direction, cp);
 		
-		String instructions01 = scan.next();
-		Instruction i1 = new Instruction(instructions01);
+		String instructions = scan.next();
+		Instruction i1 = Nasa.instruction(instructions);
 		
-		for (int j = 0; j < i1.getInstruction().split("").length; j++) {
-			String instruction = i1.getInstruction().split("")[j];
-			
-			if ("R".equals(instruction)) {
-				p1.rightMove();
-			} else if ("L".equals(instruction)) {
-				p1.leftMove();
-			} else if ("M".equals(instruction)) {
-				p1.forward(cp.getAxisX(), cp.getAxisY());
-			}
-		}
+		p1.recieverInstructions(i1);
+		
+		
+		
+		
 
     	
     	
-    	int initX02 = scan.nextInt();
-    	int initY02 = scan.nextInt();
-    	String direction02 = scan.next();
-		Probe p2 = new Probe(initX02, initY02, direction02);
-		
-		String instructions02 = scan.next();
-    	Instruction i2 = new Instruction(instructions02);
-		
-		
-		for (int i = 0; i < i2.getInstruction().split("").length; i++) {
-			String instruction = i2.getInstruction().split("")[i];
-			
-			if ("R".equals(instruction)) {
-				p2.rightMove();
-			} else if ("L".equals(instruction)) {
-				p2.leftMove();
-			} else if ("M".equals(instruction)) {
-				p2.forward(cp.getAxisX(), cp.getAxisY());
-			}
-		}
-		
-		System.out.println("=====");
-		
+//    	int initX02 = scan.nextInt();
+//    	int initY02 = scan.nextInt();
+//    	String direction02 = scan.next();
+//		Probe p2 = new Probe(initX02, initY02, direction02);
+//		
+//		String instructions02 = scan.next();
+//    	Instruction i2 = new Instruction(instructions02);
+//		
+//		
+//		for (int i = 0; i < i2.getInstruction().split("").length; i++) {
+//			String instruction = i2.getInstruction().split("")[i];
+//			
+//			if ("R".equals(instruction)) {
+//				p2.rightMove();
+//			} else if ("L".equals(instruction)) {
+//				p2.leftMove();
+//			} else if ("M".equals(instruction)) {
+//				p2.forward(cp.getAxisX(), cp.getAxisY());
+//			}
+//		}
+//		
+//		System.out.println("=====");
+//		
 		System.out.println(p1.getInitX() + " " + p1.getInitY() + " " + p1.getDirection());
-		System.out.println(p2.getInitX() + " " + p2.getInitY() + " " + p2.getDirection());
+//		System.out.println(p2.getInitX() + " " + p2.getInitY() + " " + p2.getDirection());
 		
 
 
--- java/exploringMars/src/main/java/br/com/elo7/exploringMars/probe/Probe.java
+++ java/exploringMars/src/main/java/br/com/elo7/exploringMars/probe/Probe.java
@@ -11,24 +11,32 @@ package br.com.elo7.exploringMars.probe;
 
 import java.util.ArrayList;
 
+import br.com.elo7.exploringMars.dto.CartesianPlan;
+import br.com.elo7.exploringMars.dto.Instruction;
+
 public class Probe {
 	public int initX;
 	public int initY;
 	public String direction;
 	private ArrayList<String> windRose;
+	private CartesianPlan cartesianPlan;
 	
 	
-	public Probe(int initX, int initY, String direction) {
+	public Probe(int initX, int initY, String direction, CartesianPlan cartesianPlan) {
 		this.setInitX(initX);
 		this.setInitY(initY);
 		this.setDirection(direction);
+		this.cartesianPlan = cartesianPlan;
 		
 		this.windRose = new ArrayList<String>();
 		windRose.add("N");
+		windRose.add("NE");
 		windRose.add("E");
+		windRose.add("SE");
 		windRose.add("S");
+		windRose.add("SO");
 		windRose.add("W");
-		
+		windRose.add("NO");
 	}
 	
 	/** Método para mover a sonda em frente 
@@ -64,6 +72,27 @@ public class Probe {
                 this.setInitX(this.getInitX()-1);
             }
 		}
+		
+		if ("NE".equals(this.getDirection())) {
+			this.setInitX(this.getInitX()+1);
+			this.setInitY(this.getInitY()+1);
+		}
+		
+		if ("SE".equals(this.getDirection())) {
+			this.setInitX(this.getInitX()+1);
+			this.setInitY(this.getInitY()-1);
+		}
+		
+		if ("SO".equals(this.getDirection())) {
+			this.setInitX(this.getInitX()-1);
+			this.setInitY(this.getInitY()-1);
+		}
+		
+		if ("NO".equals(this.getDirection())) {
+			this.setInitX(this.getInitX()-1);
+			this.setInitY(this.getInitY()+1);
+		}
+		
 	}
 	
 	
@@ -71,7 +100,7 @@ public class Probe {
 	 */	
 	public void rightMove() {
 		int indexDirection = this.windRose.indexOf(direction) + 1;
-		if (indexDirection == 4) {
+		if (indexDirection == 8) {
 			indexDirection = 0;
 		}
 		this.setDirection((String) this.windRose.get(indexDirection));
@@ -82,9 +111,23 @@ public class Probe {
 	public void leftMove() {
 		int indexDirection = this.windRose.indexOf(direction) - 1;
 		if (indexDirection == -1) {
-			indexDirection = 3;
+			indexDirection = 7;
 		}
 		this.setDirection((String) this.windRose.get(indexDirection));
+	}
+	
+	public void recieverInstructions(Instruction instructions) {
+		for (int j = 0; j < instructions.getInstruction().split("").length; j++) {
+			String instruction = instructions.getInstruction().split("")[j];
+			
+			if ("R".equals(instruction)) {
+				this.rightMove();
+			} else if ("L".equals(instruction)) {
+				this.leftMove();
+			} else if ("M".equals(instruction)) {
+				this.forward(cartesianPlan.getAxisX(), cartesianPlan.getAxisY());
+			}
+		}
 	}
 	
 
